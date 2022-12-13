f26531daa411cac2a5d61844e2fe111e23562854
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2022-12-13 21:21:09 +0530

    feat: Add extension function on Iterable entity to split it on a given boolean condition

--- src/main/kotlin/extensions/Iterables.kt
+++ src/main/kotlin/extensions/Iterables.kt
@@ -14,3 +14,33 @@ fun Iterable<Long>.product(): Long = reduce { acc: Long, next: Long -> acc * nex
  * Returns the difference of all [Int] values in the [Iterable] entity.
  */
 fun Iterable<Int>.difference(): Int = reduce { acc: Int, next: Int -> acc - next }
+
+/**
+ * Extension function on [Iterable] entity of type [T] to split the iterable collection
+ * when determined by the [predicate]. If the split item needs to be retained, set [retainSplitItem] to `true`.
+ *
+ * @return [Collection] of split [Collection]s of type [T].
+ */
+fun <T> Iterable<T>.splitWhen(retainSplitItem: Boolean = false, predicate: (T) -> Boolean): Collection<Collection<T>> {
+    val result = mutableListOf<MutableList<T>>()
+    val appendItem: (T) -> Unit = { item ->
+        try {
+            result.last() += item
+        } catch (e: NoSuchElementException) {
+            result += mutableListOf(item)
+        }
+    }
+
+    this.iterator().forEach { item: T ->
+        if (predicate(item)) {
+            if (retainSplitItem) {
+                appendItem(item)
+            }
+            result += mutableListOf<T>()
+        } else {
+            appendItem(item)
+        }
+    }
+
+    return result
+}
