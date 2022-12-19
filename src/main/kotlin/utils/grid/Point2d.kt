4a45dba2a02d2b9d0db0becb7d0288be5ed61598
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2022-12-19 23:04:45 +0530

    refactor: Make Point2d utility to support equals function

--- src/main/kotlin/utils/grid/Point2d.kt
+++ src/main/kotlin/utils/grid/Point2d.kt
@@ -4,5 +4,21 @@ open class Point2d<T>(val xPos: T, val yPos: T) where T : Comparable<T>, T : Num
 
     fun toCoordinateList(): List<T> = listOf(xPos, yPos)
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Point2d<*>) return false
+
+        if (xPos != other.xPos) return false
+        if (yPos != other.yPos) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = xPos.hashCode()
+        result = 31 * result + yPos.hashCode()
+        return result
+    }
+
     override fun toString(): String = "($xPos, $yPos)"
 }
