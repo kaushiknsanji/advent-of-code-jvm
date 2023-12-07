0a31092b4eb2b7a407364d97dcd934faacf42910
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2023-12-07 20:25:54 +0530

    refactor: Use 'fold' logic for 'splitWhen' extension function and make it return Iterable instead of Collection

--- src/main/kotlin/extensions/Iterables.kt
+++ src/main/kotlin/extensions/Iterables.kt
@@ -19,28 +19,18 @@ fun Iterable<Int>.difference(): Int = reduce { acc: Int, next: Int -> acc - next
  * Extension function on [Iterable] entity of type [T] to split the iterable collection
  * when determined by the [predicate]. If the split item needs to be retained, set [retainSplitItem] to `true`.
  *
- * @return [Collection] of split [Collection]s of type [T].
+ * @return [Iterable] of split [Iterable]s of type [T].
  */
-fun <T> Iterable<T>.splitWhen(retainSplitItem: Boolean = false, predicate: (T) -> Boolean): Collection<Collection<T>> {
-    val result = mutableListOf<MutableList<T>>()
-    val appendItem: (T) -> Unit = { item ->
-        try {
-            result.last() += item
-        } catch (e: NoSuchElementException) {
-            result += mutableListOf(item)
-        }
-    }
-
-    this.iterator().forEach { item: T ->
-        if (predicate(item)) {
-            if (retainSplitItem) {
-                appendItem(item)
+fun <T> Iterable<T>.splitWhen(retainSplitItem: Boolean = false, predicate: (T) -> Boolean): Iterable<Iterable<T>> =
+    this.fold(mutableListOf(mutableListOf())) { acc: MutableList<MutableList<T>>, item: T ->
+        acc.apply {
+            if (predicate(item)) {
+                if (retainSplitItem) {
+                    this.last().add(item)
+                }
+                this.add(mutableListOf())
+            } else {
+                this.last().add(item)
             }
-            result += mutableListOf<T>()
-        } else {
-            appendItem(item)
         }
     }
-
-    return result
-}
