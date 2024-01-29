686a37ec9f67cad2c2786f04337db54828195da9
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2024-01-29 16:24:01 +0530

    feat: Add Math utility functions for Fibonacci, Factorial, Permutations and Combinations computation

--- src/main/kotlin/utils/Math.kt
+++ src/main/kotlin/utils/Math.kt
@@ -1,5 +1,7 @@
 package utils
 
+import extensions.product
+
 /**
  * Returns the [Int] result of the Greatest Common Divisor of [this] and [other] values.
  */
@@ -19,6 +21,92 @@ fun Int.lcm(other: Int): Int = computeLcm(this, other)
  * Returns the [Long] result of the Least Common Multiple of [this] and [other] values.
  */
 fun Long.lcm(other: Long): Long = computeLcm(this, other)
+
+/**
+ * Generates and returns an [IntArray] of Fibonacci series for the given [nth number][number].
+ *
+ * @throws IllegalArgumentException when [number] is negative.
+ */
+fun generateFibonacci(number: Int): IntArray = if (number < 0) {
+    throw IllegalArgumentException(
+        "(number=$number < 0)! Cannot find or generate Fibonacci of negative number."
+    )
+} else {
+    IntArray(number + 1) { index ->
+        if (index < 2) {
+            // Fibonacci for 0 and 1 will be 0 and 1 respectively
+            index
+        } else {
+            // Save -1 as a temporary Fibonacci value for indexes in the series yet to be computed
+            -1
+        }
+    }.let { fibonacciArray: IntArray ->
+        // Compute remaining values in the series when given nth number is greater than 1
+        if (number > 1) {
+            (2..number).forEach { index ->
+                fibonacciArray[index] = fibonacciArray[index - 1] + fibonacciArray[index - 2]
+            }
+        }
+
+        // Return the computed Fibonacci series
+        fibonacciArray
+    }
+}
+
+/**
+ * Finds and returns the Fibonacci number for the given [nth number][number].
+ */
+fun findFibonacci(number: Int): Int = generateFibonacci(number)[number]
+
+/**
+ * Returns Factorial of the given [number].
+ *
+ * @throws IllegalArgumentException when [number] is negative.
+ */
+fun factorial(number: Int): Long = if (number < 0) {
+    throw IllegalArgumentException("(number=$number < 0)! Cannot evaluate a Factorial of negative number.")
+} else if (number == 0) {
+    // Factorial of 0 is 1
+    1L
+} else {
+    (1L..number).product()
+}
+
+/**
+ * Returns total number of possible Permutations without repetition for [totalItems] to choose from
+ * and [selectCount] of items chosen.
+ *
+ * @throws IllegalArgumentException when [selectCount] is greater than [totalItems].
+ */
+fun findTotalPermutationsWithoutRepetition(totalItems: Int, selectCount: Int): Long =
+    if (selectCount > totalItems) {
+        throw IllegalArgumentException(
+            "(r=$selectCount > n=$totalItems)! Can only choose up to $totalItems items."
+        )
+    } else if (selectCount == totalItems) {
+        // If all items are chosen, then the Factorial of total items gives the Permutations possible
+        factorial(totalItems)
+    } else {
+        (totalItems.toLong() downTo (totalItems - selectCount + 1)).product()
+    }
+
+/**
+ * Returns total number of possible Combinations without repetition for [totalItems] to choose from
+ * and [selectCount] of items chosen.
+ *
+ * @throws IllegalArgumentException when [selectCount] is greater than [totalItems].
+ */
+fun findTotalCombinationsWithoutRepetition(totalItems: Int, selectCount: Int): Long =
+    if (selectCount > totalItems) {
+        throw IllegalArgumentException(
+            "(r=$selectCount > n=$totalItems)! Can only choose up to $totalItems items."
+        )
+    } else if (selectCount == totalItems) {
+        // If all items are chosen, then only 1 Combination is possible
+        1L
+    } else {
+        findTotalPermutationsWithoutRepetition(totalItems, selectCount) / factorial(selectCount)
+    }
 
 /**
  * Function that computes and returns the [Int] result of the Greatest Common Divisor
