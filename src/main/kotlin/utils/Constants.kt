7a03338a52dd2381a100eebcdebc89e0a05a90b9
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2025-01-21 17:31:11 +0530

    feat: Add Kotlin Object for commonly used constants

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,119 @@
+package utils
+
+/**
+ * Kotlin Object for Commonly used constants.
+ *
+ * @author <a href="kaushiknsanji@gmail.com">Kaushik N Sanji</a>
+ */
+object Constants {
+    // Character Constants
+    const val SPACE_CHAR = ' '
+    const val COMMA_CHAR = ','
+    const val DOT_CHAR = '.'
+    const val HYPHEN_CHAR = '-'
+    const val MINUS_CHAR = '-'
+    const val PLUS_CHAR = '+'
+    const val EQUALS_CHAR = '='
+    const val ASTERISK_CHAR = '*'
+    const val PRODUCT_CHAR = '*'
+    const val HASH_CHAR = '#'
+    const val PIPE_CHAR = '|'
+    const val SLASH_CHAR = '/'
+    const val DIVIDE_CHAR = '/'
+    const val BACKSLASH_CHAR = '\\'
+    const val UNDERSCORE_CHAR = '_'
+    const val CARET_CHAR = '^'
+    const val GREATER_CHAR = '>'
+    const val LESSER_CHAR = '<'
+    const val A_CAP_CHAR = 'A'
+    const val E_CAP_CHAR = 'E'
+    const val O_CAP_CHAR = 'O'
+    const val S_CAP_CHAR = 'S'
+    const val X_CAP_CHAR = 'X'
+    const val T_SMALL_CHAR = 't'
+    const val V_SMALL_CHAR = 'v'
+    const val X_SMALL_CHAR = 'x'
+    const val Y_SMALL_CHAR = 'y'
+    const val Z_SMALL_CHAR = 'z'
+    const val QUESTION_MARK_CHAR = '?'
+    const val COLON_CHAR = ':'
+    const val SEMICOLON_CHAR = ';'
+    const val DOLLAR_CHAR = '$'
+    const val AT_CHAR = '@'
+    const val SQUARE_OPEN_BRACE_CHAR = '['
+    const val SQUARE_CLOSE_BRACE_CHAR = ']'
+    const val CURLY_OPEN_BRACE_CHAR = '{'
+    const val CURLY_CLOSE_BRACE_CHAR = '}'
+    const val LEFT_PARENTHESIS_CHAR = '('
+    const val RIGHT_PARENTHESIS_CHAR = ')'
+    const val NO_0_CHAR = '0'
+    const val NO_1_CHAR = '1'
+    const val NO_2_CHAR = '2'
+    const val NO_3_CHAR = '3'
+    const val NO_4_CHAR = '4'
+    const val NO_5_CHAR = '5'
+    const val NO_6_CHAR = '6'
+    const val NO_7_CHAR = '7'
+    const val NO_8_CHAR = '8'
+    const val NO_9_CHAR = '9'
+
+    // String Constants
+    const val SPACE_STRING = " "
+    const val COMMA_STRING = ","
+    const val DOT_STRING = "."
+    const val HYPHEN_STRING = "-"
+    const val MINUS_STRING = "-"
+    const val PLUS_STRING = "+"
+    const val EQUALS_STRING = "="
+    const val ASTERISK_STRING = "*"
+    const val PRODUCT_STRING = "*"
+    const val HASH_STRING = "#"
+    const val PIPE_STRING = "|"
+    const val SLASH_STRING = "/"
+    const val DIVIDE_STRING = "/"
+    const val BACKSLASH_STRING = "\\"
+    const val UNDERSCORE_STRING = "_"
+    const val CARET_STRING = "^"
+    const val GREATER_STRING = ">"
+    const val LESSER_STRING = "<"
+    const val A_CAP_STRING = "A"
+    const val E_CAP_STRING = "E"
+    const val O_CAP_STRING = "O"
+    const val S_CAP_STRING = "S"
+    const val X_CAP_STRING = "X"
+    const val T_SMALL_STRING = "t"
+    const val V_SMALL_STRING = "v"
+    const val X_SMALL_STRING = "x"
+    const val Y_SMALL_STRING = "y"
+    const val Z_SMALL_STRING = "z"
+    const val QUESTION_MARK_STRING = "?"
+    const val COLON_STRING = ":"
+    const val SEMICOLON_STRING = ";"
+    const val DOLLAR_STRING = "$"
+    const val AT_STRING = "@"
+    const val SQUARE_OPEN_BRACE_STRING = "["
+    const val SQUARE_CLOSE_BRACE_STRING = "]"
+    const val CURLY_OPEN_BRACE_STRING = "{"
+    const val CURLY_CLOSE_BRACE_STRING = "}"
+    const val LEFT_PARENTHESIS_STRING = "("
+    const val RIGHT_PARENTHESIS_STRING = ")"
+    const val DOUBLE_PIPE = "||"
+    const val RIGHT_ARROW = "->"
+    const val LEFT_ARROW = "<-"
+    const val NEW_LINE = "\n"
+    const val EMPTY = ""
+    const val NO_0_STRING = "0"
+    const val NO_1_STRING = "1"
+    const val NO_2_STRING = "2"
+    const val NO_3_STRING = "3"
+    const val NO_4_STRING = "4"
+    const val NO_5_STRING = "5"
+    const val NO_6_STRING = "6"
+    const val NO_7_STRING = "7"
+    const val NO_8_STRING = "8"
+    const val NO_9_STRING = "9"
+    const val AND_GATE = "AND"
+    const val XOR_GATE = "XOR"
+    const val OR_GATE = "OR"
+
+}
