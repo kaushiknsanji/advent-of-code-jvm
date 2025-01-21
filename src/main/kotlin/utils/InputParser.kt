fc2857ff87b18ef24e5d84fa28aa266bcb41ca52
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2025-01-21 17:32:44 +0530

    feat: Add utilities for parsing input

---
+++ src/main/kotlin/utils/InputParser.kt
@@ -0,0 +1,43 @@
+/**
+ * Kotlin file for utilities on parsing input.
+ *
+ * @author <a href="kaushiknsanji@gmail.com">Kaushik N Sanji</a>
+ */
+
+package utils
+
+import extensions.splitWhen
+
+// Regular expression to capture numbers
+private val numberRegex = """(-?\d+)""".toRegex()
+
+// Regular expression for one or more whitespaces
+private val whitespacesRegex = """\s+""".toRegex()
+
+/**
+ * Splits [this] into [Iterable] of [Iterable] lines of [String] at all [lines][String]
+ * that are either Blank or Empty
+ */
+fun List<String>.splitWhenLineBlankOrEmpty(): Iterable<Iterable<String>> =
+    splitWhen { line: String -> line.isBlank() || line.isEmpty() }
+
+/**
+ * Extracts and returns a [List] of [Int] found in [this] input line
+ */
+fun String.findAllInt(): List<Int> =
+    numberRegex.findAll(this).map { matchResult ->
+        matchResult.groupValues[1].toInt()
+    }.toList()
+
+/**
+ * Extracts and returns a [List] of [Long] found in [this] input line
+ */
+fun String.findAllLong(): List<Long> =
+    numberRegex.findAll(this).map { matchResult ->
+        matchResult.groupValues[1].toLong()
+    }.toList()
+
+/**
+ * Splits [this] line by one or more whitespaces found and returns a [List] of split [String]s
+ */
+fun String.splitContentByWhitespaces(): List<String> = split(whitespacesRegex)
