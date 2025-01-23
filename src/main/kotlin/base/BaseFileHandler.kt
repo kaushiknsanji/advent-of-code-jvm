52750a5872e73809b955e23035667a7b56a28e06
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2025-01-23 14:29:56 +0530

    fix: Edge cases from previous change
    Allows problem classes to be kept somewhere nested under the year package while restricting class name to begin as Day, and if problem classes are kept in their respective day package, then irrespective of what their class name is, day packages are restricted to be placed directly under the year package

--- src/main/kotlin/base/BaseFileHandler.kt
+++ src/main/kotlin/base/BaseFileHandler.kt
@@ -19,6 +19,7 @@ abstract class BaseFileHandler {
         private const val SAMPLE_FILE_NAME = "sample"
         private const val ACTUAL_TEST_FILE_NAME = "test"
         private const val DAY_IN_PATH = "day"
+        private const val YEAR_IN_PATH = "year"
     }
 
     /**
@@ -53,25 +54,80 @@ abstract class BaseFileHandler {
             )
 
     /**
-     * Returns [Package specific resources][getResourcesDirectoryForPackage] directory if problem classes
-     * are kept in their respective day package under the year package; else returns the
-     * Class name specific resources directory using the Class name retrieved from [getClassName]
+     * Returns the [package][File] from [this] package specific [File] directory that
+     * starts with the name [packageNameStart].
+     *
+     * @throws IllegalStateException when required package having name starting with [packageNameStart] could not
+     * be found from [this] package.
+     */
+    private fun File.getPackage(packageNameStart: String): File {
+        // Package reference for finding the required package
+        var packageFile = this
+
+        // Repeat till we find the required package
+        while (!packageFile.name.startsWith(packageNameStart)) {
+            // Move to parent package when required package is not yet found
+            packageFile = packageFile.parentFile
+        }
+
+        // Ensure the required package is found
+        check(packageFile.name.startsWith(packageNameStart)) {
+            "Required package having name starting with '$packageNameStart' is not " +
+                    "found in the path (${this.path})"
+        }
+
+        // Return package found
+        return packageFile
+    }
+
+    /**
+     * Returns the day package specific resources directory if problem classes are kept in their
+     * respective day package under the year package; else returns the Class name specific resources directory
+     * using the Class name retrieved from [getClassName].
+     *
+     * @throws IllegalStateException when problem classes are kept in their respective day package but not
+     * directly found under the year package; or when problem classes are kept somewhere nested in the year package
+     * but does not have a class name that starts as 'Day'.
      */
     private fun getResourcesDirectoryForClass(): File {
         // Package specific resources directory
         val packageFile = getResourcesDirectoryForPackage()
+
+        // Get the year package from package specific resources directory
+        val yearPackageFile = packageFile.getPackage(YEAR_IN_PATH)
 
         // Class name with support for several problem classes of the same day with valid
         // class names of the format "Day1", "Day1_2", "Day1_3" and so on, all belonging to the
         // same day "Day1" as per the example, enabled by considering class name up till underscore if any
         val className = getClassName().lowercase().substringBefore(UNDERSCORE_CHAR)
 
-        // Return package specific resources directory if problem classes are kept in their respective day package
-        // under the year package; else return the class name specific resources directory
-        return if (packageFile.absolutePath.contains(DAY_IN_PATH)) {
-            packageFile
+        // Return the day package specific resources directory if problem classes are kept in their
+        // respective day package under the year package; else return the class name specific resources directory
+        return if (packageFile.path.contains(DAY_IN_PATH)) {
+            // When problem classes are kept in their respective day package
+
+            // Get the day package from package specific resources directory
+            val dayPackageFile = packageFile.getPackage(DAY_IN_PATH)
+
+            // Ensure that the day package is the child of the year package
+            check(dayPackageFile.parentFile == yearPackageFile) {
+                "Day Package '${dayPackageFile.name}' should be directly present under " +
+                        "Year Package '${yearPackageFile.name}'"
+            }
+
+            // Return the day package specific resources directory
+            dayPackageFile
         } else {
-            packageFile.resolve(className)
+            // When problem classes are kept somewhere nested in the year package
+
+            // Ensure that the class name starts as 'Day'
+            check(className.startsWith(DAY_IN_PATH)) {
+                "Class Name should always start with '${DAY_IN_PATH.replaceFirstChar(Char::uppercaseChar)}', " +
+                        "but was '${className.replaceFirstChar(Char::uppercaseChar)}'"
+            }
+
+            // Return the class name specific resources directory
+            yearPackageFile.resolve(className)
         }
     }
 
